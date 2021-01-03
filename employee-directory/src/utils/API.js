5de1335579df6910e1d886f5758f67e96b8d9267
alligatormonday <joey@joeyjepson.com>
2021-01-02 20:19:39 -0700

    finished making API call, they said whats up

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const URL = "https://randomuser.me/api/?results=100&nat=US"
+
 // Export an object containing methods we'll use for accessing the RandomAPI
-export default {
-    getTeam: () => {
-        return axios.get("https://randomuser.me/api/?results=100&nat=US");
-    }
-};
+const API = {
+    getTeam: () => axios.get(URL)
+}
+
+export default API;
