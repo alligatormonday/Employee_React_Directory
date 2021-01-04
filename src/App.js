b73e8cfe034f97077a0a9d1a093ac40c11c416a5
alligatormonday <joey@joeyjepson.com>
2021-01-03 18:33:09 -0700

    created sort

--- src/App.js
+++ src/App.js
@@ -11,11 +11,49 @@ function App() {
   // Getter and Setter for results & search
   const [results, setResults] = useState([])
   const [search, setSearch] = useState("")
+  const [sort, setSort] = useState(true)
 
   // API call and setting the results to the API call, empty array makes this equivalent to componentOnMount
   useEffect(() => {
     API.getTeam().then(res => setResults(res.data.results))
   }, [])
+
+  // Variable/function for sorting a-z
+  let alphaSort = function (prop, arr) {
+    arr.sort(function (a, b) {
+      if (a.name[prop] < b.name[prop]) {
+        return -1;
+      } else if (a.name[prop] > b.name[prop]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  };
+
+  // Variable/function for sorting z-a
+  let betaSort = function (prop, arr) {
+    arr.sort(function (a, b) {
+      if (a.name[prop] < b.name[prop]) {
+        return 1;
+      } else if (a.name[prop] > b.name[prop]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (sort) {
+      alphaSort("first", results)
+    }
+    else {
+      betaSort('first', results)
+    }
+
+  }, [sort])
+
 
   return (
     <div className="App">
@@ -23,7 +61,7 @@ function App() {
       <Container>
         <input type="text" className='searchBar' value={search} onChange={(event) => { setSearch(event.target.value) }}>
         </input>
-        <Table>
+        <Table sort={sort} setSort={setSort}>
           {search.length < 1 ? results.map((result, i) => (
             <TableContent
               name={result.name.first + " " + result.name.last}
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,19 @@
-
-
 import React from "react";
 
-function Table({ children }) {
+function Table(props) {
+   
     return (
         <table className="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col">Name</th>
+                    <th scope="col">Name <span className=''><svg className="" onClick={() => {props.setSort(!props.sort)}} width="1rem" height="1rem" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg></span></th>
                     <th scope="col">Email</th>
                     <th scope="col">Phone</th>
                     <th scope="col">Picture</th>
                 </tr>
             </thead>
             <tbody>
-                {children}
+                {props.children}
             </tbody>
         </table>
     )
@@ -22,3 +21,4 @@ function Table({ children }) {
 }
 
 export default Table;
+
